700f01248091b6d6fb57f37b997aa83b9cd0f2e9
adamvankat <vankatadam@gmail.com>
2021-05-07 10:25:11 +0200

    Simple Todo list done (no css)

---
+++ BlazorToDoList/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class TodoItem
+{
+    public string Title { get; set; }
+    public bool IsDone { get; set; }
+}
+
